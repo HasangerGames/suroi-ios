efb749a052d0f6debb611596ed65666a71520a06
TomatoPotato <nimaigos108@gmail.com>
2025-01-25 13:49:20 -0800

    Fixed launch issues

--- Suroi/SceneDelegate.swift
+++ Suroi/SceneDelegate.swift
@@ -5,7 +5,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-        guard let _ = (scene as? UIWindowScene) else { return }
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+
+            let window = UIWindow(windowScene: windowScene)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            window.rootViewController = storyboard.instantiateInitialViewController()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        guard let url = userActivity.webpageURL else { return }
+        print("Universal Link opened: \(url)")
     }
 }
